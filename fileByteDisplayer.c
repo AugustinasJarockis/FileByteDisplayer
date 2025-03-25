e67187e862f48e8ac12fc159815be724f31755bd
AugustinasJarockis <augustinas.jarockis@gmail.com>
2025-03-25 23:16:51 +0200

    Add start, end and line count flags

--- fileByteDisplayer.c
+++ fileByteDisplayer.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -7,14 +9,38 @@ int main(int argc, char **argv)
     FILE* file = fopen(argv[1], "rb");
 
     int linesToRead = -1;
-    if (argc == 3) {
-        linesToRead = atoi(argv[2]);
+    int linesToSkip = 0;
+    int endLine = -1;
+
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "-s") == 0 && i + 1 < argc)
+            linesToSkip = atoi(argv[i + 1]);
+        else if (strcmp(argv[i], "-e") == 0 && i + 1 < argc)
+            endLine = atoi(argv[i + 1]);
+        else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc)
+            linesToRead = atoi(argv[i + 1]);
+        else{
+            printf("ERROR: Unrecognized argument: %s\n", argv[i]);
+            continue;
+        }
+        i++;
     }
+
+    if (endLine != -1) {
+        if (linesToRead != -1)
+            linesToRead = linesToRead < endLine - linesToSkip + 1 ? linesToRead : endLine - linesToSkip + 1;
+        else
+            linesToRead = endLine - linesToSkip + 1;
+    }
+
+    fseek(file, linesToSkip * 16, SEEK_SET);
 
     int linesRead = 0;
 
     while(!feof(file) && (linesToRead <= 0 || linesRead < linesToRead))
     {
+        printf("%3d. ", linesRead + linesToSkip);
+
         int cutOffVal = 16;
         for(int i = 0; i < 16; i++)
         {
