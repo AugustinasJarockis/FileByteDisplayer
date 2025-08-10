a00c5c8838db13951afa54335e6d2cfa64ac37f7
AugustinasJarockis <augustinas.jarockis@gmail.com>
2025-08-10 16:12:10 +0300

    Add basic input sanitation

--- fileByteDisplayer.c
+++ fileByteDisplayer.c
@@ -4,25 +4,54 @@
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("ERROR: Path to the file must be specified\n");
+        return 1;
+    }
 
     FILE* file = fopen(argv[1], "rb");
+
+    if (file == NULL) {
+        printf("ERROR: Specified file does not exist\n");
+        return 2;
+    }
 
     int linesToRead = -1;
     int linesToSkip = 0;
     int endLine = -1;
     int lineSize = 16;
 
-    unsigned char bytes[lineSize];
 
     for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i], "-s") == 0 && i + 1 < argc)
+        if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
             linesToSkip = atoi(argv[i + 1]);
-        else if (strcmp(argv[i], "-e") == 0 && i + 1 < argc)
+            if (linesToSkip < 0) {
+                printf("WARNING: Number of skipped lines cannot be negative. No lines will be skipped\n");
+                linesToSkip = 0;
+            }
+        }
+        else if (strcmp(argv[i], "-e") == 0 && i + 1 < argc) {
             endLine = atoi(argv[i + 1]);
-        else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc)
+            if (endLine < 1) {
+                printf("WARNING: The final line line has to be at least the first one. Reverting to default behaviour\n");
+                endLine = -1;
+            }
+        }
+        else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
             linesToRead = atoi(argv[i + 1]);
+            if (linesToRead < 1) {
+                printf("WARNING: At least one line must be read. Reverting to default behaviour\n");
+                linesToRead = -1;
+            }
+        }
         else if (strcmp(argv[i], "-l") == 0 && i + 1 < argc)
+        {
             lineSize = atoi(argv[i + 1]);
+            if (lineSize < 1) {
+                printf("WARNING: Each line must contain at least one byte. Reverting to default (16)\n");
+                lineSize = 16;
+            }
+        }
         else{
             printf("ERROR: Unrecognized argument: %s\n", argv[i]);
             continue;
@@ -40,6 +69,7 @@ int main(int argc, char **argv)
     fseek(file, linesToSkip * lineSize, SEEK_SET);
 
     int linesRead = 0;
+    unsigned char bytes[lineSize];
 
     while(!feof(file) && (linesToRead <= 0 || linesRead < linesToRead))
     {
